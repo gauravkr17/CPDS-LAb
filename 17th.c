83a84d381596796993d0819a95e964b42101a3ea
Gaurav <gauravkr7265@gmail.com>
2023-02-15 19:13:16 +0530

    solve pblm 17

--- 17th.c
+++ 17th.c
@@ -3,5 +3,15 @@
             //  Question 17 - Write a program to print factorial of 5.
             
 void main(){
+    int i=1, n, fact=1;
+
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    while (i<=n)
+    {
+        fact = fact*i;
+        i++;
+    }
+    printf("Factorial of %d is %d", n , fact);
     
 }
