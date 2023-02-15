608cda8bb8b806cbfb7db7e4be00ef05838478c2
Gaurav <gauravkr7265@gmail.com>
2023-02-15 18:50:07 +0530

    solve pblm no 16

--- 16th.c
+++ 16th.c
@@ -3,6 +3,19 @@
             // Question 16 - Write a program to print 10 natural munbers and sum it.
             
 void main(){
+    int i=1, n , sum=0;
+
+    printf("Enter a positive number : ");
+    scanf("%d" , &n);
+
+    while (i<=n)
+    {
+        printf("%d " , i);
+        sum += i;
+        i++;
+    }
+    printf("\nSum of first %d natural numbers is %d" , n,sum);
     
+
     
 }
