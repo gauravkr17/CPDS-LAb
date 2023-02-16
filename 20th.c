deaadf836c8c01517507e88e9356dd06ae4f1a9f
Gaurav <gauravkr7265@gmail.com>
2023-02-16 08:06:48 +0530

    sloved pblm 20

--- 20th.c
+++ 20th.c
@@ -3,5 +3,16 @@
             //  Question 20 - Write a program to print the power of the number.
             
 void main(){
+    int i = 1, p , b , pow = 1;
+    printf("Enter Base then space then Power: ");
+    scanf("%d %d" , &b , &p);
+
+    while (i<=p)
+    {
+        pow = pow * b;
+        i++;
+    }
+    printf("The %d power of base %d is %d", p, b, pow);
+
     
 }
