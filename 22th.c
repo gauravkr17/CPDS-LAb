8ae87602d7f807b49131af6ba2340b07cf599ed0
Gaurav <gauravkr7265@gmail.com>
2023-02-19 18:31:13 +0530

    solved

--- 22th.c
+++ 22th.c
@@ -3,5 +3,21 @@
             // Question 22 - write a program to print n Fibonacci series.
 
 void main(){
+    int n, i;
+    long long int a=0, b=1, c=0;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series : \n");
+
+    for (i = 1; i <= n; i++)
+    {
+        printf("%lld\n", a);
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    
 
 }
