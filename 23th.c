3816c419f66ca7a11949f24b665f2b6e64dfe8cd
Gaurav <gauravkr7265@gmail.com>
2023-02-27 20:01:52 +0530

    pblm solved

--- 23th.c
+++ 23th.c
@@ -3,6 +3,26 @@
             //  Write a program in c to print pyramid.
 
 void main(){
+    int rows, i, j, k = 0;
+
+    printf("Enter number of rows: ");
+    scanf("%d" , &rows);
+
+    for ( i = 1; i <= rows; ++i, k=0)
+    {
+        for ( j = 1; j <= rows-i; ++j)
+        {
+            printf(" ");
+
+        }
+        while (k!=2*i-1)
+        {
+            printf("*");
+            ++k;
+        }
+        printf("\n");
+    }
     
+
 
 }
