241a4613c47cf23a22562a0b213d7fe785a2420f
Gaurav <gauravkr7265@gmail.com>
2023-02-27 20:10:29 +0530

    explaination added

--- 23th.c
+++ 23th.c
@@ -26,3 +26,23 @@ void main(){
 
 
 }
+
+
+
+
+/*  Explaination:
+
+    1. the user is prompted to enter the number of rows for the pyramid pattern to be printed.
+
+    2. the outer for loop is used to iterate through each row of the pyramid.
+        the loop variable "i" represents the current row number
+
+    3. the inner for loop is used to print spaces before the asterisks to create the pyramid shape.
+        the number of spaces printed in each row is equal to "row-i"
+
+    4. the while loop prints the asterisks. the number of asterisks printed in each row is equal to "2*i-1"
+
+    5. after printing all the characters in a row, a newline is added to move to the next line.
+
+    6. the program ends after printing all the rows of the pyramid
+*/
