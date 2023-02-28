d12cd66635cd3006ee0805ff365a8acc15158e0b
Gaurav <gauravkr7265@gmail.com>
2023-02-28 22:24:38 +0530

    code updated

--- 5th_LAB.c
+++ 5th_LAB.c
@@ -4,19 +4,43 @@ void main(){
 
     // Question 05 - Write a program to convert days into years, months and days.
 
+    // int days;
+    // printf("Enter days to convert it : ");
+    // scanf("%d", &days);
+
+    // int year = days/365;
+    // printf("%d Year,", year);
+
+    // int year1 = days%365;
+    // int month = year1/30;
+    // printf(" %d month," , month);
+
+    // int month1 = year1%30;
+    // printf(" %d days", month1);
+
+
+
     int days;
     printf("Enter days to convert it : ");
     scanf("%d", &days);
 
     int year = days/365;
-    printf("%d Year,", year);
+    printf("%d Year," , year);
 
-    int year1 = days%365;
-    int month = year1/30;
-    printf(" %d month," , month);
+    days=days%365;
+    int month = days/30;
+    printf("%d Months," , month);
 
-    int month1 = year1%30;
-    printf(" %d days", month1);
+    days = days%30;
+    int weeks = days/7;
+    printf("%d Weeks," , weeks);
+
+    days = days%7;
+    printf("%d Days" , days);
+    
+
+
+    
 
     
 }
