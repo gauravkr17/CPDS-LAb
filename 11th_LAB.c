49f0ed0244369665c74b71af0c225d5a8fab66b2
Gaurav <gauravkr7265@gmail.com>
2023-03-04 18:50:26 +0530

    Capital alphabet condition added

--- 11th_LAB.c
+++ 11th_LAB.c
@@ -8,7 +8,7 @@ void main(){
     printf("Enter an alphabet : ");
     scanf("%c" , &ch);
 
-    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+    if (ch == 'a' ||ch == 'A' || ch == 'e' ||ch == 'E' || ch == 'i' ||ch == 'I' || ch == 'o' ||ch == 'O' || ch == 'u' ||ch == 'U'){
         printf("%c is a vowel\n" , ch);
     }
     else{
